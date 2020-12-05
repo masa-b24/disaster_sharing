d7d6915ae781f31fb2eba28e1caea8df577192e2
“masa-b24” <take3611.pg@gmail.com>
2020-12-05 11:37:24 +0900

    Create profile-image & Add profile edit pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,19 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to home_path, notice: "プロフィールが更新されました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:avatar)
+    end
+
+
 end
