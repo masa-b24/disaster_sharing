66cd4bfe0ab2c897099f1103b2229ade87489f1c
“masa-b24” <take3611.pg@gmail.com>
2020-12-14 23:29:18 +0900

    Change view home-page. Add tab-function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,18 @@ class StaticPagesController < ApplicationController
     @posts = @posts.where('title LIKE :keyword OR content LIKE :keyword', keyword: "%#{params[:keyword]}%") if params[:keyword].present?
     @posts = @posts.tagged_with(params[:tag]) if params[:tag].present?
   end
+
+  def home_new
+    @posts = Post.sorted
+    @posts = @posts.where(prefecture_id: params[:prefecture_id]) if params[:prefecture_id].present? 
+    @posts = @posts.where('title LIKE :keyword OR content LIKE :keyword', keyword: "%#{params[:keyword]}%") if params[:keyword].present?
+    @posts = @posts.tagged_with(params[:tag]) if params[:tag].present?
+  end
+
+  def home_request
+    @posts = Post.where(request_help: true)
+    @posts = @posts.where(prefecture_id: params[:prefecture_id]) if params[:prefecture_id].present? 
+    @posts = @posts.where('title LIKE :keyword OR content LIKE :keyword', keyword: "%#{params[:keyword]}%") if params[:keyword].present?
+    @posts = @posts.tagged_with(params[:tag]) if params[:tag].present?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
    }
 
   get 'profiles/show'
-  root to: 'static_pages#top'
   get '/home', to: 'static_pages#home'
+  get '/home_new', to: 'static_pages#home_new'
+  get '/home_request', to: 'static_pages#home_request'
   resource :users
   resource :profiles
   resources :posts
