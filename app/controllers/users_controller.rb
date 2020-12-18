9ba4d791b00f79b7635dfaae62fc10bd58cf9c37
“masa-b24” <take3611.pg@gmail.com>
2020-12-18 21:13:38 +0900

    Change resource: user to resources: user, change mypage url

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:mypage]
+
   def show
+    @user = User.find(params[:id])
     @post = current_user.posts
   end
+
+  #def mypage
+    #redirect_to user_path(current_user)
+  #end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,14 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
    }
-
+  
+  get '/mypage' => 'users#mypage'
   get 'profiles/show'
   get '/home', to: 'static_pages#home'
   get '/home_new', to: 'static_pages#home_new'
   get '/home_request', to: 'static_pages#home_request'
   resource :users
+  resources :users
   resource :profiles
   resources :posts do
     resources :comments, only: [:create]
